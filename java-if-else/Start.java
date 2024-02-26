6c47bc21e9dc6a3ddc7fbdd2f5ddfbb376b43e62
sheikh-niloy <sheikh.niloy.info@gmail.com>
2024-02-27 01:09:20 +0600

    java-if-else

---
+++ java-if-else/Start.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Start {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) 
+    {      
+        Scanner obj1 = new Scanner (System.in);
+        int N = obj1.nextInt();
+        if (N % 2!=0)
+        {
+            System.out.println("Weird");
+        }  
+        if (N % 2==0 && N>=2 && N<=5)
+        {
+            System.out.println("Not Weird");
+        }    
+        if (N % 2==0 && N>=6 && N<=20 )
+        {
+            System.out.println("Weird");
+        }
+        if (N % 2==0 && N>20)
+        {
+            System.out.println("Not Weird");
+        }
+    }
+}
