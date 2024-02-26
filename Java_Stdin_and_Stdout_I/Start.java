5fdb11e8b21ae36fc11c2aeda4b17bc6762ad07b
sheikh-niloy <sheikh.niloy.info@gmail.com>
2024-02-27 00:38:17 +0600

    Java_Stdin_and_Stdout_I

--- Java_Stdin_and_Stdout_I/Start.java
+++ Java_Stdin_and_Stdout_I/Start.java
@@ -1,7 +1,17 @@
 import java.lang.*;
-public class Start 
+import java.util.Scanner;
+
+public class Start
 {
-    public static void main(String[] args) {
-        System.out.println("Broo!!");
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
